7da025a6fdd3a35b0a887ee320a0d4491ddff51c
Ben Wizen <benwizen@gmail.com>
2019-03-21 12:14:49 +0200

    support for static arrays ([]) + removed ctor regex group

--- main_generator.py
+++ main_generator.py
@@ -1,10 +1,11 @@
 import re
-import json
 import string
+import json
 from jsonStepper import json_navigator
 from random_name import rand_name
 from collections import Counter
 from random import *
+from functools import reduce
 
 
 def parse_header(header_path, req_amount):
@@ -25,9 +26,9 @@ def parse_header(header_path, req_amount):
             if re.match('class\s(\w+)', s):
                 class_name = s.split(' ')[1]
             # Members
-            extract(members, '\\t(?P<type>\w+)\s+(?P<name>\w+);', s, False)
+            extract(members, '\\t(?P<type>\w+)\s+(?P<name>\w+)(?P<arr_size>(\[\d+\])*);', s, False)
             # Ctors
-            extract(ctors, '\\t(?P<ctor>\w+)(\((?P<args_str>.*)\));', s, True)
+            extract(ctors, '\\t(\w+)(\((?P<args_str>.*)\));', s, True)
             # Methods and setters
             extract(methods, '\\t(?P<ret_type>\w+) (?P<name>\w+)(\((?P<args_str>.*)\));', s, True)
 
@@ -49,9 +50,22 @@ def extract(out, regex_pattern, string, has_args):
                     arg_split.pop(0)
                 arg['type'] = arg_split[0].replace('&', '')
                 arg['name'] = arg_split[1]
+                if arg_split[1].find('[') > -1:
+                    arg['is_arr'] = True
+                else:
+                    arg['is_arr'] = False
                 args.append(arg)
             dict['args'] = args
         out.append(dict)
+
+
+def array_generator(arr_type, el):
+    arr_str = '{'
+    for _ in range(el):
+        arr_str += f'{random_value_gen(arr_type)}, '
+    arr_str = arr_str.rstrip(', ')
+    arr_str += '}'
+    return arr_str
 
 
 def random_value_gen(type):
@@ -62,7 +76,7 @@ def random_value_gen(type):
             return round(uniform(1, 10), 2)
         elif type == 'char':
             return random.choice(string.ascii_letters)
-        elif type == 'char*':
+        elif type == 'char*' or type == 'string':
             return rand_name()
         else:
             raise Exception("Unknown type in random_value_gen")
@@ -81,13 +95,15 @@ def generate_objects(class_name, cjson, pre_objs, count=5):
             rel_ctor = ctor
             break
 
-    for obj_id in range(count):
+    for id in range(count):
         args_required = int(json_navigator('/args_required', rel_ctor))
-        obj_id = f'{class_name.lower()}{obj_id + 1}'
+        obj_id = f'{class_name.lower()}{id + 1}'
         objs.append({'class': class_name, 'obj_id': obj_id})
         obj_str = f"{class_name} {obj_id}("
         for arg_id in range(args_required):
-            m_type = json_navigator(f'/args/{arg_id}/type', rel_ctor)
+            arg = json_navigator(f'/args/{arg_id}', rel_ctor)
+            m_type = arg['type']
+            is_arr = arg['is_arr']
             if m_type not in bi_types:
                 for idx, po in enumerate(pre_objs):
                     if m_type == po['class']:
@@ -96,6 +112,14 @@ def generate_objects(class_name, cjson, pre_objs, count=5):
                             obj_str += ", "
                         pre_objs.pop(idx)
                         break
+            elif is_arr:
+                m = re.search('(\[\d+\])+', arg['name'])
+                arr_size = m.group()
+                arr_len = reduce(lambda a, b: int(a) * int(b), re.findall('\d', arr_size))
+                arg_name = re.sub('\[\d+\]', '', arg['name'])
+                arg_name += f"_{obj_id[0]}_{id + 1}"
+                arr = f"{arg['type']} {arg_name}{arr_size} = {array_generator('int', int(arr_len))};\n"
+                obj_str = arr + obj_str + f"{arg_name}, "
             else:
                 rnd = random_value_gen(m_type)
                 obj_str += str(rnd)
@@ -169,6 +193,7 @@ def main_generator(paths):
     final_str = ""
     pre_objs = []
     total_cls_json = classes_json_gen(paths)
+    # print(json.dumps(total_cls_json,indent=3))
     depths = check_depths(total_cls_json)
     prereq = prereq_for_classes(total_cls_json)
     for depth in range(1, max(depths.keys()) + 1):
@@ -179,14 +204,15 @@ def main_generator(paths):
                                               pre_objs,
                                               max(prereq[cls],
                                                   json_navigator(f'/{cls}/req_amount', total_cls_json)))
-        pre_objs += objs
-        final_str += objs_str
+            pre_objs += objs
+            final_str += objs_str
     return final_str
 
 
 if __name__ == '__main__':
-    paths = {r'C:\Users\User\Desktop\CPP\Ex2\T2\T2\Point.h': 10,
-             r'C:\Users\User\Desktop\CPP\Ex2\T2\T2\Line.h': 3,
-             r'C:\Users\User\Desktop\CPP\Ex2\T2\T2\Triangle.h': 3}
+    paths = {r'C:\Users\User\Desktop\CPP\Ex2\T2\T2\Point.h': 5,
+             r'C:\Users\User\Desktop\CPP\Ex2\T2\T2\Line.h': 2,
+             r'C:\Users\User\Desktop\CPP\Ex2\T2\T2\Triangle.h': 2}
 
+    main_generator(paths)
     print(main_generator(paths))
